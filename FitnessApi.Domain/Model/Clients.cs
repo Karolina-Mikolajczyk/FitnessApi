6017cb968ff0909c04c8221249b900ec7b05adc0
Karolina Mikolajczyk <karolina.kujawska@interia.pl>
2024-10-19 20:29:02 +0200

    Add project files.

---
+++ FitnessApi.Domain/Model/Clients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApi.Domain.Model
+{
+   public class Clients
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AddressEmail { get; set; }
+        public string NumberPhone { get; set; }
+    }
+}
